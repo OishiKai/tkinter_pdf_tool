d55e776994fac0474afc21dd467ad10499c0c181
OishiKai <kai.appdveloper@gmail.com>
2025-03-30 02:03:23 +0900

    横スクロール修正

--- ui/widgets/csv_viewer.py
+++ ui/widgets/csv_viewer.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+import config.fonts as fonts
 import customtkinter as ctk
 
 
@@ -8,6 +9,7 @@ class CSVViewer(ctk.CTkFrame):
         super().__init__(master, fg_color="transparent", corner_radius=0)
         self.pack(fill="both", expand=True, pady=20)
         self.result = result
+        font = fonts.get_fonts()
 
         top_frame = ctk.CTkFrame(self, fg_color="transparent", corner_radius=0)
         top_frame.pack(fill="x", padx=10)
@@ -19,19 +21,23 @@ class CSVViewer(ctk.CTkFrame):
 
         # ttk のスタイルを設定（フォントサイズを統一）
         style = ttk.Style()
-        style.configure("Treeview", font=("Arial", 10))  # 本体のフォント
+        style.configure("Treeview", font=font["description"])  # 本体のフォント
         style.configure(
-            "Treeview.Heading", font=("Arial", 12, "bold")
+            "Treeview.Heading",
+            font=(font["title"]),
         )  # ヘッダーのフォント
 
         # Treeviewウィジェットを追加（テーブル表示用）
         self.tree = ttk.Treeview(self, show="headings")  # ヘッダーを表示
         self.tree.pack(fill="both", expand=True, padx=10, pady=10)
 
-        # スクロールバーを追加
-        self.scroll_y = ttk.Scrollbar(self, orient="vertical", command=self.tree.yview)
-        self.tree.configure(yscroll=self.scroll_y.set)
-        self.scroll_y.pack(side="right", fill="y")
+        # 横スクロールバーを追加
+        self.x_scrollbar = ttk.Scrollbar(
+            self, orient="horizontal", command=self.tree.xview
+        )
+        self.tree.configure(xscrollcommand=self.x_scrollbar.set)
+        self.x_scrollbar.pack(fill="x", side="bottom")
+
         self.after(100, self.setup_csv)
 
     def setup_csv(self):
